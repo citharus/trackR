af87bdb10f989365b5e05ddd7d292f88b71ab35d
citharus <citharus@protonmail.com>
2021-10-16 00:15:33 +0200

    :ambulance: Replaced old `table.entries`

--- src/trckr/database/database.py
+++ src/trckr/database/database.py
@@ -58,7 +58,7 @@ class Database:
     def save(self) -> None:
         for table in self:
             with open(self.path / table, 'wb') as file:
-                data: bytes = bson.dumps(self[table].entries)
+                data: bytes = bson.dumps(dict(self[table]))
                 file.write(data)
 
     def load(self) -> None:
