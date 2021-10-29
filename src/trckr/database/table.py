3cd6b7761fa7ea026ff8df844b8ecca60146926c
citharus <citharus@protonmail.com>
2021-10-29 23:49:47 +0200

    :recycle: Set default `query` limit to unlimited

--- src/trckr/database/table.py
+++ src/trckr/database/table.py
@@ -63,5 +63,5 @@ class Table:
         except KeyError:
             raise Exception(f'Entry "{key}" does not exist.')
 
-    def query(self, key: Callable = None, limit: int = 10) -> dict:
+    def query(self, key: Callable = None, limit: int = -1) -> dict:
         return dict(sorted(self.__entries.items(), key=key)[:limit])
