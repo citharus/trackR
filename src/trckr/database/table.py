823c2ba2af77d9368cd104f22af9f9b6de8e44fd
citharus <citharus@protonmail.com>
2021-11-08 14:57:00 +0100

    :fire: Removed `__len__`

--- src/trckr/database/table.py
+++ src/trckr/database/table.py
@@ -30,9 +30,6 @@ class Table:
         self.name: str = name
         self.__entries: dict = entries or dict()
 
-    def __len__(self) -> int:
-        return len(self.__entries)
-
     def __iter__(self) -> Iterable['Table']:
         return iter(self.__entries)
 
