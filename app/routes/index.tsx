276376d91974726df86f5adc4baf4425685e2765
citharus <citharus@protonmail.com>
2022-08-15 22:21:15 +0200

    Added `header` with name and placeholder logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import styleUrl from "~/styles/app.css";
 
@@ -7,5 +8,19 @@ export const links: LinksFunction = () => {
 }
 
 export default function Index() {
-    return
+    return (
+        <div>
+            <header className="py-1 sm:px-4 bg-gray-50 outline outline-1 outline-gray-100">
+                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                    <Link className="flex items-center" to="/" prefetch="intent">
+                        <h1>
+                            <span className="mr-3 text-3xl">📈</span>
+                            <span className="self-center text-3xl font-semibold text-transparent bg-clip-text
+                                             bg-gradient-to-br from-rose-600 to-orange-300">trackR</span>
+                        </h1>
+                    </Link>
+                </div>
+            </header>
+        </div>
+    );
 }
